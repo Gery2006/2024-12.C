c5c5bd480b5da1e401859a35332471d751c142fd
Gery2006 <gloknerkegergelyke@gmail.com>
2024-09-25 09:41:36 +0200

    tobbelem

---
+++ TobbElem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TobbElem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ide jönnek a kódok.
+        }
+    }
+}
