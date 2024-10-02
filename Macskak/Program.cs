b4e4b78a993d824b3d102520095bc39f70db3b87
Gery2006 <gloknerkegergelyke@gmail.com>
2024-10-02 08:14:01 +0200

    enum példa

---
+++ Macskak/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Macskak
+{
+    class Program
+    {
+        enum Animals {macska, kutya, tigris, farkas};
+        static void Main(string[] args)
+        {
+            Animals b = Animals.farkas;
+            Animals c = Animals.macska;
+            if (b == Animals.farkas)
+            {
+                Console.WriteLine("A b állat farkas");
+            }
+            if (c==Animals.macska)
+            {
+                Console.WriteLine("A c állat macska");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
