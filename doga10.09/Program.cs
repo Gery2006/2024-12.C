d8d268f9b0bad6c50d646037a3173ac55f45e3d4
Gery2006 <gloknerkegergelyke@gmail.com>
2024-11-06 06:59:40 +0100

    .

---
+++ doga10.09/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doga10._09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("A szám páros.");
+            }
+            else
+            {
+                Console.WriteLine("A szám páratlan.");          
+            }
+            Console.ReadKey();
+        }
+    }
+}
