a28d52fe70d86626f51b12526ac63588ead84cc0
Gery2006 <gloknerkegergelyke@gmail.com>
2024-12-04 11:53:02 +0100

    .

---
+++ doga 12.04/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doga_12._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem az évet: ");
+
+            string ev = Console.ReadLine();
+            int szam;
+            bool szam1 = int.TryParse(ev, out szam);
+
+            if (szam1)
+            {
+                Console.WriteLine($"{ev} egy szám.");
+            }
+            else
+            {
+                Console.WriteLine($"{ev} nem egy szám.");
+            }
+
+            Console.WriteLine("Kérem a halál évszámát: ");
+
+            int szam2;
+            bool szam2 = int.TryParse(ev, out szam);
+
+            if (szam2)
+            {
+                Console.WriteLine($"{ev} egy szám.");
+            }
+            else
+            {
+                Console.WriteLine($"{ev} nem egy szám.");
+            }
+            int halal = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Add meg a nevet: ");
+            string nev = Convert.ToString(Console.ReadLine());
+
+            int idos = halal - ev;
+            Console.WriteLine($"Ennyi idősen halt meg: {nev}: {idos}");
+
+            Console.WriteLine("Kérem az országot: ");
+            string orszag=Convert.ToString(Console.ReadLine());
+            Console.ReadKey();
+
+            
+        }
+    }
+}
+
+
+  
